5e16a4ffd8e734bc570488f19f265540ff5d3566
tclayto4 <tclayto4@charlotte.edu>
2025-11-08 16:00:13 -0500

    n

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './styles/default.css';
+import App from './App.jsx';
 
-import "./index.css";
-import "./styles/default.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
